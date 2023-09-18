94cdc7ea2c36ef760fd7a2f4ef3c62042e6e3c5b
Luiz Carmo <124007941+luizcarmoo@users.noreply.github.com>
2023-09-18 14:44:57 -0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
 // Variables
-
 let todoItems = [];
 const todoInput = document.querySelector(".todo-input");
 const completedTodosDiv = document.querySelector(".completed-todos");
@@ -19,7 +18,8 @@ window.onload = () => {
 // Get the content typed into the input
 todoInput.onkeyup = (e) => {
   let value = e.target.value.replace(/^\s+/, "");
-  if (value && e.keyCode === 13) { // Enter
+  if (value && e.keyCode === 13) {
+    // Enter
     addTodo(value);
 
     todoInput.value = "";
@@ -30,9 +30,9 @@ todoInput.onkeyup = (e) => {
 // Add todo
 function addTodo(text) {
   todoItems.push({
-        id: Date.now(),
-        text,
-        completed: false
+    id: Date.now(),
+    text,
+    completed: false,
   });
 
   saveAndRender();
@@ -40,13 +40,13 @@ function addTodo(text) {
 
 // Remove todo
 function removeTodo(id) {
-  todoItems = todoItems.filter(todo => todo.id !== Number(id));
+  todoItems = todoItems.filter((todo) => todo.id !== Number(id));
   saveAndRender();
 }
 
 // Mark as completed
 function markAsCompleted(id) {
-  todoItems = todoItems.filter(todo => {
+  todoItems = todoItems.filter((todo) => {
     if (todo.id === Number(id)) {
       todo.completed = true;
     }
@@ -60,8 +60,8 @@ function markAsCompleted(id) {
 }
 
 // Mark as uncompleted
-function markAsUncompleted(id) { }
-  todoItems = todoItems.filter(todo => {
+function markAsUncompleted(id) {
+  todoItems = todoItems.filter((todo) => {
     if (todo.id === Number(id)) {
       todo.completed = false;
     }
@@ -70,7 +70,7 @@ function markAsUncompleted(id) { }
   });
 
   saveAndRender();
-
+}
 
 // Save in localstorage
 function save() {
@@ -79,14 +79,14 @@ function save() {
 
 // Render
 function render() {
-  let unCompletedTodos = todoItems.filter(item => !item.completed);
-  let completedTodos = todoItems.filter(item => item.completed);
+  let unCompletedTodos = todoItems.filter((item) => !item.completed);
+  let completedTodos = todoItems.filter((item) => item.completed);
 
   completedTodosDiv.innerHTML = "";
   uncompletedTodosDiv.innerHTML = "";
 
   if (unCompletedTodos.length > 0) {
-    unCompletedTodos.forEach(todo => {
+    unCompletedTodos.forEach((todo) => {
       uncompletedTodosDiv.append(createTodoElement(todo));
     });
   } else {
@@ -96,7 +96,7 @@ function render() {
   if (completedTodos.length > 0) {
     completedTodosDiv.innerHTML = `<div class='completed-title'>Completed (${completedTodos.length} / ${todoItems.length})</div>`;
 
-    completedTodos.forEach(todo => {
+    completedTodos.forEach((todo) => {
       completedTodosDiv.append(createTodoElement(todo));
     });
   }
@@ -113,7 +113,7 @@ function createTodoElement(todo) {
   // Create todo list container
   const todoDiv = document.createElement("div");
   todoDiv.setAttribute("data-id", todo.id);
-  todoDiv.className = "todo-item";
+  todoDiv.className = "todo-item"; // Adicione a classe "todo-item"
 
   // Create todo item text
   const todoTextSpan = document.createElement("span");
@@ -129,21 +129,27 @@ function createTodoElement(todo) {
   };
 
   // Delete button for list
-  const todoRemoveBtn = document.createElement('a');
+  const todoRemoveBtn = document.createElement("a");
   todoRemoveBtn.href = "#";
   todoRemoveBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M18 6l-12 12"></path>
-        <path d="M6 6l12 12"></path>
-    </svg>`;
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M18 6l-12 12"></path>
+      <path d="M6 6l12 12"></path>
+  </svg>`;
   todoRemoveBtn.onclick = (e) => {
     let id = e.target.closest(".todo-item").dataset.id;
     removeTodo(id);
 
-    todoTextSpan.prepend(todoInputCheckbox);
-    todoDiv.appendChild(todoTextSpan);
-    todoDiv.appendChild(todoRemoveBtn);
+    // Atualize a renderização após remover o item
+    render();
 
-    return todoDiv;
+    return false; // Adicione esta linha para evitar que o link siga o href
   };
+
+  // Agora, você deve anexar os elementos criados à div "todoDiv"
+  todoDiv.appendChild(todoInputCheckbox);
+  todoDiv.appendChild(todoTextSpan);
+  todoDiv.appendChild(todoRemoveBtn);
+
+  return todoDiv; // Retorna a div criada com a classe "todo-item"
 }
